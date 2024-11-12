7b6f8e73d6d1234ee03968d46e471c1cccb08de2
Lilly Helbling <lilly@helbling.uk>
2024-11-12 20:08:28 +0000

    Set up ffmpeg

---
+++ src/lib/ffmpeg.ts
@@ -0,0 +1,30 @@
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { get, writable } from "svelte/store";
+
+const ffmpegStore = writable<FFmpeg | undefined>(undefined);
+
+const ffmpegLoadingState = writable<'uninitialised' | 'loading' | 'ready'>('uninitialised');
+
+
+export function loadFFmpeg(){
+  if (get(ffmpegLoadingState) !== 'uninitialised'){
+    return;
+  }
+
+  ffmpegLoadingState.set('loading');
+
+  const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.6/dist/esm";
+  const a = new FFmpeg();
+
+  a.load({
+    coreURL: baseURL + '/ffmpeg-core.js',
+    wasmURL: baseURL + '/ffmpeg-core.wasm'
+  }).then(() => {
+    ffmpegStore.set(a);
+    ffmpegLoadingState.set('ready');
+    return a;
+  }).catch((err) => {
+    console.log(err);
+    ffmpegLoadingState.set('uninitialised');
+  })
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+</script>
+
+<h1>Video Converter</h1>
+<input type="file" accept="video/*" />
